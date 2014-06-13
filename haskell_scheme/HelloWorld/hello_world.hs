cf3138b118857585f647c376efb664beb47fc4ae
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-13 15:17:28 -0700

    Fixing do syntax

--- haskell_scheme/HelloWorld/hello_world.hs
+++ haskell_scheme/HelloWorld/hello_world.hs
@@ -3,7 +3,7 @@ import System.Environment
 
 main :: IO ()
 main = do
-    putStrLn ("What is your name:")
-    args <- getLine
-    putStrLn ("Hello, " ++ args)
-    -- putStrLn (show ((read (args !! 0)) + (read (args !! 1))))
+         putStrLn ("What is your name:")
+         args <- getLine
+         putStrLn ("Hello, " ++ args)
+         -- putStrLn (show ((read (args !! 0)) + (read (args !! 1))))
--- haskell_scheme/Parsing/parser.hs
+++ haskell_scheme/Parsing/parser.hs
@@ -13,4 +13,7 @@ readExpr :: String -> String
 readExpr input = case parse symbol "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found value"
-    
+
+main :: IO ()
+main = do
+         args 
