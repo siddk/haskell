c31689abb4924c900fe133eba12b04c71994c037
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-13 14:41:59 -0700

    Typecast to int, add numbers

--- haskell_scheme/HelloWorld/hello_world.hs
+++ haskell_scheme/HelloWorld/hello_world.hs
@@ -4,4 +4,4 @@ import System.Environment
 main :: IO ()
 main = do
     args <- getArgs
-    putStrLn ("Hello, " ++ (show ((read (args !! 0)) + (read (args !! 1)))))
+    putStrLn (show ((read (args !! 0)) + (read (args !! 1))))
