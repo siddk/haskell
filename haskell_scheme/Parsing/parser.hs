ab9d0592d8ed7aa1ae87bfe6df5d63f51cb01ac0
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-13 15:09:30 -0700

    Documenting readExpr, setting type function

--- haskell_scheme/Parsing/parser.hs
+++ haskell_scheme/Parsing/parser.hs
@@ -5,3 +5,8 @@ import System.Environment
 -- Define a parser that recognizes symbols allowed in Scheme identifiers
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+-- Create function readExpr, passes input string to Parsec Parse function,
+-- which takes a Parser (symbol), a name for the input ("Lisp") and the
+-- input itself (input).
+readExpr :: String -> String
