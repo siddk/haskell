a28e5207d86d0f6cf23fb0ee250b156a31c813a6
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-13 15:06:57 -0700

    Documenting scheme form parser

--- haskell_scheme/Parsing/parser.hs
+++ haskell_scheme/Parsing/parser.hs
@@ -2,4 +2,6 @@ module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 
+-- Define a parser that recognizes symbols allowed in Scheme identifiers
 symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
