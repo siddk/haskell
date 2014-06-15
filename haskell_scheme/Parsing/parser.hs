a44749c5bcd7ae16bf14d4bb2973e2dafa9bee8f
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-15 10:48:22 -0700

    New number parser stub

--- haskell_scheme/Parsing/parser.hs
+++ haskell_scheme/Parsing/parser.hs
@@ -42,8 +42,10 @@ parseAtom = do
 
 --Create LispVal Parser to parse out Numbers. Uses liftM to lift Parser
 --String monad off of many1 digit
+--Edit: New Parser with do notation and >>= operator
 parseNumber :: Parser LispVal
-parseNumber = liftM (Number . read) $ many1 digit
+-- parseNumber = liftM (Number . read) $ many1 digit
+
 
 --Create general LispVal Expression parser
 parseExpr = parseAtom
