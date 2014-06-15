190b643920870eb9a02e5a6824bd8f2a268f4ba3
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-15 11:09:32 -0700

    Compile

--- haskell_scheme/Parsing/parser.hs
+++ haskell_scheme/Parsing/parser.hs
@@ -68,10 +68,11 @@ parseExpr = parseAtom
          <|> parseString
          <|> parseNumber
          <|> parseQuoted
-         <|> do char '('
-                        x <- try parseList <|> parseDottedList
-                        char ')'
-                        return x
+         <|> do
+                char '('
+                x <- try parseList <|> parseDottedList
+                char ')'
+                return x
 
 -- Create function readExpr, passes input string to Parsec Parse function,
 -- which takes a Parser (parseExpr), a name for the input ("Lisp")
