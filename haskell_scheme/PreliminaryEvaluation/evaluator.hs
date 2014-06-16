d74d1f9b1c909e33ccec1c36779c578863d0e8c8
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-16 13:38:48 -0700

    Update readExpr

--- haskell_scheme/PreliminaryEvaluation/evaluator.hs
+++ haskell_scheme/PreliminaryEvaluation/evaluator.hs
@@ -101,7 +101,7 @@ eval (List [Atom "quote", val]) = val
 --Create function readExpr, passes input string to Parsec Parse function,
 --which takes a Parser (parseExpr), a name for the input ("Lisp")
 --and the input itself (input).
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
     Left err -> String $ "No match: " ++ show err
     Right val -> val
