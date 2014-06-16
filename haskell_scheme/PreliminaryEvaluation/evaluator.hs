1f25b94690174d63889e910f9e354b1e24e9ba95
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-16 13:36:00 -0700

    Preliminary primitive eval

--- haskell_scheme/PreliminaryEvaluation/evaluator.hs
+++ haskell_scheme/PreliminaryEvaluation/evaluator.hs
@@ -99,6 +99,13 @@ readExpr input = case parse parseExpr "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found " ++ show val
 
+--Setup primitive type eval
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
+
 -- Main function, reads in command line args, executes readExpr on args
 main :: IO ()
 main = do
