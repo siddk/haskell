0c976f4fbc3722cc49cfbcc6e9896e243bf1de23
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-18 16:29:08 -0700

    Adding REPL loop, function, updating main

--- haskell_scheme/REPL/repl.hs
+++ haskell_scheme/REPL/repl.hs
@@ -300,9 +300,22 @@ evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr 
 evalAndPrint :: String -> IO ()
 evalAndPrint expr =  evalString expr >>= putStrLn
 
--- Main function, reads in command line args, executes readExpr on args
+--Loop logic for REPL
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+   result <- prompt
+   if pred result
+      then return ()
+      else action result >> until_ pred prompt action
+
+--REPL IO function
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
+
+-- Main function, REPL
 main :: IO ()
-main = do
-         args <- getArgs
-         evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-         putStrLn $ extractValue $ trapError evaled
+main = do args <- getArgs
+          case length args of
+               0 -> runRepl
+               1 -> evalAndPrint $ args !! 0
+               otherwise -> putStrLn "Program takes only 0 or 1 argument"
