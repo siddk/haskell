86e0ffd9e2ccfd9709ca6adbfad592f4f905dd74
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-17 19:35:47 -0700

    Add if clause to eval

--- haskell_scheme/Evaluation/eval.hs
+++ haskell_scheme/Evaluation/eval.hs
@@ -99,6 +99,11 @@ eval val@(Number _) = return val
 eval val@(Bool _) = return val
 eval (List [Atom "quote", val]) = return val
 eval (List (Atom func : args)) = mapM eval args >>= apply func
+eval (List [Atom "if", pred, conseq, alt]) =
+     do result <- eval pred
+        case result of
+             Bool False -> eval alt
+             otherwise  -> eval conseq
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
 --Setup apply function, to apply a function to a series of arguments --> Primitive functionality
