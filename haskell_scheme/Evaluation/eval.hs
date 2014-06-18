b1aee331d0307c5c3d40d72683b637a6e4b88145
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-17 19:26:07 -0700

    Define boolBinop

--- haskell_scheme/Evaluation/eval.hs
+++ haskell_scheme/Evaluation/eval.hs
@@ -137,6 +137,14 @@ numericBinop op           []  = throwError $ NumArgs 2 []
 numericBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
 numericBinop op params        = mapM unpackNum params >>= return . Number . foldl1 op
 
+--Define boolBinop
+boolBinop :: (LispVal -> ThrowsError a) -> (a -> a -> Bool) -> [LispVal] -> ThrowsError LispVal
+boolBinop unpacker op args = if length args /= 2
+                             then throwError $ NumArgs 2 args
+                             else do left <- unpacker $ args !! 0
+                                      right <- unpacker $ args !! 1
+                                      return $ Bool $ left `op` right
+
 --Define unpackNum
 unpackNum :: LispVal -> ThrowsError Integer
 unpackNum (Number n) = return n
