1456cdc90b65aeab834ac1c3c1895c18c6a963cb
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-22 21:50:17 -0700

    Practicing functions within functions

--- learnyouahaskell/baby.hs
+++ learnyouahaskell/baby.hs
@@ -2,4 +2,5 @@
 doubleMe x = x + x
 
 --Function that takes two values, returns sum of doubling
-doubleUs x y = x*2 + y*2
+--doubleUs x y = x*2 + y*2
+doubleUs x y = doubleMe x + doubleMe y --function composition
