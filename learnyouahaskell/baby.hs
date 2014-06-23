878c4f45415349e0a69f8012bb22faa5625277c8
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-22 21:52:24 -0700

    If statement practice

--- learnyouahaskell/baby.hs
+++ learnyouahaskell/baby.hs
@@ -4,3 +4,8 @@ doubleMe x = x + x
 --Function that takes two values, returns sum of doubling
 doubleUs x y = doubleMe x + doubleMe y
 
+--If statement practice
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x*2
+
