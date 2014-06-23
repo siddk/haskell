0ac59bdc721199b94090d6509afc519df9867ea5
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-22 23:21:27 -0700

    syntax

--- learnyouahaskell/baby.hs
+++ learnyouahaskell/baby.hs
@@ -1,13 +1,16 @@
 -- Function that doubles a value
 doubleMe x = x + x
 
+
 -- Function that takes two values, returns sum of doubling
 doubleUs x y = doubleMe x + doubleMe y
+
 
 -- If statement practice
 doubleSmallNumber x = if x > 100
                         then x
                         else x*2
+
 
 -- Basic Syntax Notes
 -- let keyword: defines a name/value (variable) in ghci
