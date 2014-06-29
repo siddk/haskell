2a78c638cfe9848ba6ecdc87358c0e63a89996bf
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-29 00:19:33 -0700

    Problem 3 solved

--- 99_problems/99_problems.hs
+++ 99_problems/99_problems.hs
@@ -8,3 +8,7 @@ myLast x = last x
 -- Problem 2: Find the last but one element of a list.
 myButLast :: [x] -> x
 myButLast x = last $ init x
+
+-- Problem 3: Find the K'th element of a list. The first element in the list is number 1.
+elementAt :: [value] -> Int -> value
+elementAt list num = list !! (num - 1)
