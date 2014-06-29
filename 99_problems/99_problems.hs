e8d5df694aa7696df4241b329a4ea36c05a61b8b
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-29 00:04:53 -0700

    Starting problem 1 stub

---
+++ 99_problems/99_problems.hs
@@ -0,0 +1,4 @@
+-- 99 Haskell Problems, based on 99 Prolog Probems and 99 LISP Problems
+-- 99_problems.hs contains problem statements, and solution scripts
+
+-- Problem 1: Find the last element of a list.
