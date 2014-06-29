1e0754fca7b14965b52cd8598819e033ab4d6034
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-29 00:13:10 -0700

    Problem 2 stub

--- 99_problems/99_problems.hs
+++ 99_problems/99_problems.hs
@@ -4,3 +4,7 @@
 -- Problem 1: Find the last element of a list.
 myLast :: [x] -> x
 myLast x = last x
+
+-- Problem 2: Find the last but one element of a list.
+myButLast :: [x] -> x
+myLast x = last $ init x
