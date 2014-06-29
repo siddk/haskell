430be521ab6b105a32bff81fb46d560be161f9a7
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-06-29 00:24:22 -0700

    Problem 4 solved (these are a bit easy with standard libraries... maybe consider not using them)

--- 99_problems/99_problems.hs
+++ 99_problems/99_problems.hs
@@ -1,5 +1,5 @@
 -- 99 Haskell Problems, based on 99 Prolog Probems and 99 LISP Problems
--- 99_problems.hs contains problem statements, and solution scripts
+-- 99_problems.hs contains problem statements, and solution scripts (using standard lib)
 
 -- Problem 1: Find the last element of a list.
 myLast :: [x] -> x
@@ -12,3 +12,7 @@ myButLast x = last $ init x
 -- Problem 3: Find the K'th element of a list. The first element in the list is number 1.
 elementAt :: [value] -> Int -> value
 elementAt list num = list !! (num - 1)
+
+-- Problem 4: Find the number of elements of a list.
+myLength :: [x] -> Int
+myLength x = length x
