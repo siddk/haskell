55dca808fea9bb825fe1d84fbdaf7e0d6f3a24bb
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-04 17:32:06 -0700

    Functions w/o type

--- haskellfastandhard/type_functions.hs
+++ haskellfastandhard/type_functions.hs
@@ -1,8 +1,14 @@
 -- Declare type with functions, introduction to basic
 -- function definition.
 
--- Function f -> Sum squares of two numbers
+-- Function f (type declared) -> Sum squares of ints.
+-- Problem: Parameters to f must be ints.
 f :: Int -> Int -> Int
 f x y = x*x + y*y
 
-main = print (f 2 3)
+-- Function g (no type) -> Same as f
+g x y = x*x + y*y
+
+main = do
+    print (f 2 3) -- f can only handle ints
+    print (g 2.2 3) -- g can handle floats, ints, etc.
