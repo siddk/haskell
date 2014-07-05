a46f90d97a48586f21c941cfa60e5d23db347f50
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-04 18:01:29 -0700

    Function types

--- haskellfastandhard/notation.hs
+++ haskellfastandhard/notation.hs
@@ -70,3 +70,14 @@
 -- (.) the composition function
 -- (f . g) x       ⇔  f (g x)
 -- (f . g . h) x   ⇔  f (g (h x))
+
+
+-- Function declaration
+-- Defining the type of function before declaration isn't necessary, but it is considered good practice.
+
+-- x :: Int           ⇔ x is of type Int
+-- x :: a             ⇔ x can be of any type
+-- x :: Num a => a    ⇔ x can be any type a in Num type class
+-- f :: a -> b        ⇔ f is a function from a to b
+-- f :: a -> b -> c   ⇔ f is a function from a to (b→c)
+-- f :: (a -> b) -> c ⇔ f is a function from (a→b) to c
