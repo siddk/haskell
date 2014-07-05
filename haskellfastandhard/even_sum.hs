eda367da196ddc84a8fd11a96ed326abe0a9f481
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-04 18:42:43 -0700

    Version 2

--- haskellfastandhard/even_sum.hs
+++ haskellfastandhard/even_sum.hs
@@ -12,5 +12,19 @@ accumSum n l = if l == []
                         then accumSum (n+x) xs
                         else accumSum n xs
 
+-- Version 2
+-- Type class generalization, where, let helper functions
+evenSum' :: Integral a => [a] -> a
+evenSum' l = accumSum' 0 l
+    where accumSum' n l =
+            if l == []
+                then n
+                else let x = head l
+                         xs = tail l
+                     in if even x
+                        then accumSum (n+x) xs
+                        else accumSum n xs
+
 main = do
     print (evenSum [1, 2, 3, 4, 5])
+    print (evenSum' [1..5])
