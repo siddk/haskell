3149641ac1ff695c0191bdf7445230a37010e5bd
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-04 18:33:26 -0700

    First version of evenSum

---
+++ haskellfastandhard/even_sum.hs
@@ -0,0 +1,13 @@
+-- even_sum.hs
+-- Problem: Given a list of integers, return the sum of the even numbers in the list.
+
+-- Version 1
+evenSum :: [Integer] -> Integer
+evenSum l = accumSum 0 l
+accumSum n l = if l == []
+                then n
+                else let x = head l
+                         xs = tail l
+                     in if even x
+                        then accumSum n+x xs
+                        else accumSum n xs
