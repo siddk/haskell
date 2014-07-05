34860ec5c998b546580cd163740a6191a090edb2
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-04 17:57:39 -0700

    Function composition

--- haskellfastandhard/notation.hs
+++ haskellfastandhard/notation.hs
@@ -1,8 +1,10 @@
 -- Notation.hs
 -- Basic type notation, data structures
 
+
 -- Arithmetic
 -- Follows order of operations, use parentheses
+
 
 -- Logic
 -- True || False ⇒ True
@@ -10,13 +12,16 @@
 -- True == False ⇒ False
 -- True /= False ⇒ True  (/=) => not equal (diff)
 
+
 -- Powers
 -- x^n     for n an integral (understand Int or Integer)
 -- x**y    for y any kind of number (Float for example)
 
+
 -- Int vs. Integer
 -- Int is more efficient, but it is capped
 -- Integer is unbounded (i.e. factorial 1000)
+
 
 -- Lists, List creation
 -- Lists have to be singular type
@@ -34,6 +39,7 @@
 -- [2,3,5,7,11..100]       ⇔ ERROR! I am not so smart!
 -- [10,9..1]               ⇔ [10,9,8,7,6,5,4,3,2,1]
 
+
 -- Strings
 -- Strings are a list of Char
 --
@@ -45,8 +51,22 @@
 --
 -- In real code, use Data.Text to represent strings
 
+
 -- Tuples
 -- Unlike lists, tuples can have multiple types
 -- A special type of tuple is the pair (a, b)
 -- Pairs have special functions: fst, snd
 -- Otherwise, index with !!
+
+
+-- Function Composition
+-- Two operators -> $, .
+-- By default:
+-- f g h x         ⇔  (((f g) h) x)
+-- $ delays function evaluation to the right
+-- f g $ h x       ⇔  f g (h x) ⇔ (f g) (h x)
+-- f $ g h x       ⇔  f (g h x) ⇔ f ((g h) x)
+-- f $ g $ h x     ⇔  f (g (h x))
+-- (.) the composition function
+-- (f . g) x       ⇔  f (g x)
+-- (f . g . h) x   ⇔  f (g (h x))
