a4a12de8c01c796ca12b31d10f00ecda7dc8ca73
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-07 00:42:24 -0700

    Basic alias example

--- haskellfastandhard/types.hs
+++ haskellfastandhard/types.hs
@@ -4,3 +4,17 @@
 -- data Name = NameConstructor AnotherType --> does create a distinction
 -- data --> can construct structures which can be recursives
 -- deriving --> creates functions for you
+
+-- 1. You can construct your own Types with aliases/type synonyms:
+type Name  = String
+type Color = String
+showInfos :: Name ->  Color -> String
+showInfos name color =  "Name: " ++ name
+                        ++ ", Color: " ++ color
+name :: Name
+name = "Robin"
+
+color :: Color
+color = "Blue"
+
+main = putStrLn $ showInfos name color
