c6797ba61bb93a19c7a69d1b51b499735364df71
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-07 01:14:11 -0700

    Working treeFromList function

--- haskellfastandhard/types.hs
+++ haskellfastandhard/types.hs
@@ -66,3 +66,5 @@ treeFromList :: (Ord a) => [a] -> BinTree a
 treeFromList [] = Empty
 treeFromList (x:xs) = Node x (treeFromList (filter (<x) xs))
                              (treeFromList (filter (>x) xs))
+
+treeMain = print $ treeFromList [7,2,4,8]
