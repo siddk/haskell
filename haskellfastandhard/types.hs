c4e89be655395f8c82f3606661b79367b8e1fe13
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-07 00:51:50 -0700

    Data constructor

--- haskellfastandhard/types.hs
+++ haskellfastandhard/types.hs
@@ -5,16 +5,42 @@
 -- data --> can construct structures which can be recursives
 -- deriving --> creates functions for you
 
--- 1. You can construct your own Types with aliases/type synonyms:
-type Name  = String
-type Color = String
-showInfos :: Name ->  Color -> String
-showInfos name color =  "Name: " ++ name
+-- You can construct your own Types with aliases/type synonyms:
+type AliasName  = String
+type AliasColor = String
+aliasShowInfos :: AliasName ->  AliasColor -> String
+aliasShowInfos name color =  "Name: " ++ name
                         ++ ", Color: " ++ color
-name :: Name
-name = "Robin"
+aliasName :: AliasName
+aliasName = "Robin"
 
-color :: Color
-color = "Blue"
+aliasColor :: AliasColor
+aliasColor = "Blue"
 
-main = putStrLn $ showInfos name color
+aliasMain = putStrLn $ aliasShowInfos aliasName aliasColor
+-- The issue with this is that you can swap around parameters, or pass in Strings without declaring them as type Name or Color, and the program will compile and run perfectly.
+
+-- To remedy this, you can create own types with data keyword
+data DataName   = DataNameConstr String
+data DataColor  = DataColorConstr String
+
+dataShowInfos :: DataName ->  DataColor -> String
+dataShowInfos (DataNameConstr name) (DataColorConstr color) =
+      "Name: " ++ name ++ ", Color: " ++ color
+
+dataName  = DataNameConstr "Robin"
+dataColor = DataColorConstr "Blue"
+
+dataMain = putStrLn $ dataShowInfos dataName dataColor
+
+-- Syntax of the 'data' keyword:
+-- data TypeName =   ConstructorName  [types]
+--                | ConstructorName2 [types]
+--                | ...
+
+-- Or, to declare fields
+-- data DataTypeName = DataConstructor {
+--                      field1 :: [type of field1]
+--                    , field2 :: [type of field2]
+--                    ...
+--                    , fieldn :: [type of fieldn] }
