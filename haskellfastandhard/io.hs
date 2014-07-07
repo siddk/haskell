715e3ccb4b45a9188b4aab05d21af8bc49f504ad
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-07 01:21:45 -0700

    More notes on the IO block

--- haskellfastandhard/io.hs
+++ haskellfastandhard/io.hs
@@ -7,3 +7,7 @@
 --   action2 x
 --   y <- action3
 --   action4 x y
+
+-- In an IO block, each consecutive line will have an IO supertype
+
+-- Problem 1: Ask a user to enter a list of numbers. Print the sum of the numbers
