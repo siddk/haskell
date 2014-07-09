60c27e6ed362b098938c8480c115e2920a765703
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-08 20:28:28 -0700

    Potential solution

--- hackerrank_haskell/Introduction/listReplication.hs
+++ hackerrank_haskell/Introduction/listReplication.hs
@@ -2,7 +2,11 @@
 -- Given a list repeat each element of the list n times. The input and output portions will be handled automatically by the grader. You need to write a function with the recommended method signature
 
 f :: Int -> [Int] -> [Int]
-f n arr =
+f n arr = iterF n n arr []
+    where iterF n count arr list
+            | null arr = list
+            | count == 0 = iterF n n (tail arr) list
+            | otherwise = iterF n (count - 1) arr (list ++ [head arr])
 
 -- This part handles the Input and Output and can be used as it is. Do not modify this part.
 main = do
