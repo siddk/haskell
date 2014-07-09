07a8470a049f3845c10cd4950046378ac797f3f8
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-08 20:16:52 -0700

    Potential solution for updateList

--- hackerrank_haskell/Introduction/updateList.hs
+++ hackerrank_haskell/Introduction/updateList.hs
@@ -1,9 +1,8 @@
 -- Update List
 -- Update the values of a list with their absolute values. The input and output portions will be handled automatically during grading. You only need to write a function with the recommended method signature.
 
--- Enter your code here. Read input from STDIN. Print output to STDOUT
-
-f arr = -- Complete this function here
+f :: [Int] -> [Int]
+f arr = [abs x | x <- arr]
 
 -- This section handles the Input/Output and can be used as it is. Do not modify it.
 main = do
