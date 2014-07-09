355d678ea381fdac3f30867ecd04079e620aed55
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-08 20:54:23 -0700

    Working solution for e^x

--- hackerrank_haskell/Introduction/exApproximation.hs
+++ hackerrank_haskell/Introduction/exApproximation.hs
@@ -2,7 +2,6 @@
 -- The series expansion of ex is given by:
 -- 1 + x + x2/2! + x3/3! + x4/4! + .......
 -- Evaluate e^x for given values of x, by using the above expansion for the first 10 terms.
-
 solve :: Double -> Double
 solve x = rSolve 9 x
     where rSolve count x = if count == 0
