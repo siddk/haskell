365d31d620e058f08b422ebf23a7b8115a44f95e
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-08 20:25:21 -0700

    IO stub

--- hackerrank_haskell/Introduction/listReplication.hs
+++ hackerrank_haskell/Introduction/listReplication.hs
@@ -1,2 +1,10 @@
 -- List Replication
--- Given a list repeat each element of the list n times. The input and output portions will be handled automatically by the grader. You need to write a function with the recommended method signature.
+-- Given a list repeat each element of the list n times. The input and output portions will be handled automatically by the grader. You need to write a function with the recommended method signature
+
+f n arr = -- Complete this Function
+
+-- This part handles the Input and Output and can be used as it is. Do not modify this part.
+main = do
+   n <- readLn :: IO Int
+   inputdata <- getContents
+   mapM_ putStrLn $ map show $ f n $ map (read :: String -> Int) $ lines inputdata
