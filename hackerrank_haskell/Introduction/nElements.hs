d94cdb5f14d2f6e2df8fd8ffd7344ca632ceaa88
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-08 20:38:27 -0700

    Problem statement for Array of N Elements

---
+++ hackerrank_haskell/Introduction/nElements.hs
@@ -0,0 +1,2 @@
+-- Array of N Elements
+-- Create an array of N integers, where N is specified as input. This can be any array with N integers. For example, for N=4 you could return [1,1,1,1] or [1,2,3,4]
