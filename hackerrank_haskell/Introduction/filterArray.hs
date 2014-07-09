18e2d28babb308d649df261760d2759b5a0fe259
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-08 20:13:21 -0700

    Done with filter array problem

--- hackerrank_haskell/Introduction/filterArray.hs
+++ hackerrank_haskell/Introduction/filterArray.hs
@@ -2,7 +2,12 @@
 -- Filter a given array of integers and leave only that values which are less than a specified value X. The integers in the output should be in the same sequence as they were in the input.
 
 f :: Int -> [Int] -> [Int]
-f n arr = --Fill up this function
+f n arr = filterArray n arr []
+    where filterArray n arr list
+            | null arr = list
+            | n > head arr = filterArray n (tail arr) (list ++ [head arr])
+            | otherwise = filterArray n (tail arr) list
+
 
 -- The Input/Output section. You do not need to change or modify this part
 main = do
