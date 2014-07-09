c86350c34b359d4326656de8cb03c7ffca70ae30
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-08 20:44:16 -0700

    IO stub

--- hackerrank_haskell/Introduction/exApproximation.hs
+++ hackerrank_haskell/Introduction/exApproximation.hs
@@ -2,3 +2,9 @@
 -- The series expansion of ex is given by:
 -- 1 + x + x2/2! + x3/3! + x4/4! + .......
 -- Evaluate e^x for given values of x, by using the above expansion for the first 10 terms.
+
+solve :: Double -> Double
+solve x = -- Insert your code here --
+
+main :: IO ()
+main = getContents >>= mapM_ print. map solve. map (read::String->Double). tail. words
