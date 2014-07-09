c00a5a6c463b57b3801e4008fb18a4ad6b663073
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-08 20:30:49 -0700

    IO stub

--- hackerrank_haskell/Introduction/sumOdd.hs
+++ hackerrank_haskell/Introduction/sumOdd.hs
@@ -1,2 +1,9 @@
 -- Sum of Odd Elements
 -- Return sum of odd elements from an list.
+
+f arr = -- Fill up this function body
+
+-- This part handles the Input/Output and can be used as it is. Do not change or modify it.
+main = do
+   inputdata <- getContents
+   putStrLn $ show $ f $ map (read :: String -> Int) $ lines inputdata
