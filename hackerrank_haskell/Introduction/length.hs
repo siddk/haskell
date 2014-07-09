26cbddabce28e3741653982642973c91f794d588
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-08 20:19:27 -0700

    Problem statement for List Length

---
+++ hackerrank_haskell/Introduction/length.hs
@@ -0,0 +1,2 @@
+-- List Length
+-- Count the number of elements in an array without using count, size or length operators (or their equivalents). The input and output portions will be handled automatically by the grader. You only need to write a function with the recommended method signature.
