619048bd600e59037118b84070d194bb48a3293e
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2014-07-08 20:30:11 -0700

    Working solution for List Replication

--- hackerrank_haskell/Introduction/listReplication.hs
+++ hackerrank_haskell/Introduction/listReplication.hs
@@ -1,6 +1,5 @@
 -- List Replication
 -- Given a list repeat each element of the list n times. The input and output portions will be handled automatically by the grader. You need to write a function with the recommended method signature
-
 f :: Int -> [Int] -> [Int]
 f n arr = iterF n n arr []
     where iterF n count arr list
